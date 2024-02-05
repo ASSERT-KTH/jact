f601c616236a44c0fcf1d387cd39cec57f7acfd2
Jonas Sävås <jonas.savas123@gmail.com>
2024-02-05 16:01:05 +0100

    Small testing changes, printint output directory etc

--- src/main/java/jonas/maven/master/DependencyCounterMojo.java
+++ src/main/java/jonas/maven/master/DependencyCounterMojo.java
@@ -1,6 +1,11 @@
 package jonas.maven.master;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.List;
+import java.lang.ProcessBuilder;
 
 import org.apache.maven.model.Dependency;
 import org.apache.maven.plugin.AbstractMojo;
@@ -36,10 +41,17 @@ public class DependencyCounterMojo extends AbstractMojo {
     public void execute() throws MojoExecutionException, MojoFailureException {
         List<Dependency> dependencies = project.getDependencies();
 
-        for(int i = 0; i < dependencies.size(); i++){
-            System.out.println("DEPENDENCY: " + dependencies.get(i).toString());
-            System.out.println("SCOPE: " + dependencies.get(i).getScope());
+        String outputDir = project.getBuild().getOutputDirectory();
+        System.out.println("OUTPUT DIRECTORY: " + outputDir);
+
+        mvnVersion();
+
+
+        for (Dependency dependency : dependencies) {
+            System.out.println("DEPENDENCY: " + dependency.toString());
+            System.out.println("SCOPE: " + dependency.getScope());
         }
+
 
 
         long numDependencies = dependencies.stream()
@@ -47,6 +59,52 @@ public class DependencyCounterMojo extends AbstractMojo {
                 .count();
 
         getLog().info("Number of dependencies: " + numDependencies);
+    }
+
+
+
+    void mvnVersion(){
+        try {
+            // Command to be executed
+            String command = "mvn --version";
+
+            // Create a process builder with a shell
+            ProcessBuilder processBuilder = new ProcessBuilder("cmd", "/c", command); // For Windows
+            // ProcessBuilder processBuilder = new ProcessBuilder("/bin/bash", "-c", command); // For Linux
+
+            // Redirect error stream to output stream
+            processBuilder.redirectErrorStream(true);
+
+            // Start the process
+            Process process = processBuilder.start();
+
+            // Get the input stream of the process
+            InputStream inputStream = process.getInputStream();
+
+            // Read the output
+            String output = readInputStream(inputStream);
+
+            // Wait for the process to complete
+            int exitCode = process.waitFor();
+
+            // Print the output
+            System.out.println("Exit Code: " + exitCode);
+            System.out.println("Output:\n" + output);
+        } catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private static String readInputStream(InputStream inputStream) throws IOException {
+        // Read the input stream and convert it to a string
+        try (BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream))) {
+            StringBuilder output = new StringBuilder();
+            String line;
+            while ((line = reader.readLine()) != null) {
+                output.append(line).append("\n");
+            }
+            return output.toString();
+        }
     }
 
 }
