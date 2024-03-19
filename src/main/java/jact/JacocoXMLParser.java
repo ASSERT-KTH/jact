a18a6ef988e9f3c85fbd7d7b37d2138742f4a740
Jonas Sävås <jonas.savas123@gmail.com>
2024-03-19 13:15:36 +0100

    Adds the improves xml writing with formatted output, still need to fix unnecessary newlines for package reports

--- src/main/java/jact/JacocoXMLParser.java
+++ src/main/java/jact/JacocoXMLParser.java
@@ -1,21 +1,25 @@
 package jact;
 
 import jact.plugin.XmlReportMojo;
+import org.jsoup.Jsoup;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 import org.w3c.dom.NodeList;
 
 import javax.xml.parsers.DocumentBuilder;
 import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.transform.OutputKeys;
 import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerFactory;
 import javax.xml.transform.dom.DOMSource;
 import javax.xml.transform.stream.StreamResult;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.*;
 
 import static jact.PackageToDependencyResolver.packageToDepPaths;
-import static jact.plugin.AbstractReportMojo.*;
 
 public class JacocoXMLParser {
 
@@ -34,11 +38,18 @@ public class JacocoXMLParser {
         try {
             // Load Jacoco XML report
             File xmlFile = new File(REPORTPATH + "jacoco_report.xml"); // Currently manually generated.
+
+            // Disable DTD validation
+            System.setProperty("javax.xml.parsers.DocumentBuilderFactory", "com.sun.org.apache.xerces.internal.jaxp.DocumentBuilderFactoryImpl");
             DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
-            dbFactory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false); // Disable DTD validation
+            dbFactory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+
             DocumentBuilder dBuilder = dbFactory.newDocumentBuilder();
             Document doc = dBuilder.parse(xmlFile);
             doc.getDocumentElement().normalize();
+
+            // Format and overwrite the XML file
+            formatXml(xmlFile, doc);
 
             // Group coverage data by package name
             Map<String, Document> packageReports = new HashMap<>();
@@ -55,7 +66,7 @@ public class JacocoXMLParser {
                 String packageName = entry.getKey();
                 Document packageReport = entry.getValue();
                 String filename = packageName.replace("/", "-") + ".xml"; // Use package name for filename
-                writeXML(packageReport, REPORTPATH + "xml_reports/" + filename);
+                writePackageXMLReport(packageReport, REPORTPATH + "xml_reports/" + filename);
                 //System.out.println("filename: " + filename);
             }
 
@@ -75,11 +86,15 @@ public class JacocoXMLParser {
                 for (File file : files) {
                     // Extract filename without extension
                     String filename = file.getName().replaceAll("\\.xml$", "");
-                    System.out.println(filename);
+                    //System.out.println(filename);
                     // Call your function with the filename
                     filename = filename.replace("-", ".");
                     ProjectDependency matchedDep = packageToDepPaths(filename, dependencies, projPackagesAndClassMap, localRepoPath);
-                    System.out.println(matchedDep.getId());
+                    //System.out.println(matchedDep.getId());
+
+                    // Add the dependency usage to each matched dependency.
+                    // Create templates or a way to write the xml package to the report.
+                    //matchedDep.
                 }
             } else {
                 System.out.println("No files found in the directory.");
@@ -88,9 +103,62 @@ public class JacocoXMLParser {
             System.out.println("Directory does not exist or is not a directory.");
         }
 
-
-
     }
+
+//    public static void extractAndAddPackageTotal(String inputFilePath, ProjectDependency matchedDep, String packageName) throws IOException {
+//        try {
+//            // Read the HTML file
+//            File inputFile = new File(inputFilePath);
+//            org.jsoup.nodes.Document doc = Jsoup.parse(inputFile, "UTF-8");
+//
+//            String formattedHtml = doc.outerHtml();
+//
+//            // Write the formatted HTML back to the original file, overwriting its previous content
+//            org.apache.commons.io.FileUtils.writeStringToFile(inputFile, formattedHtml, "UTF-8");
+//        } catch (IOException e) {
+//            System.err.println("Error: " + e.getMessage());
+//            e.printStackTrace();
+//        }
+//
+//        try (BufferedReader br = new BufferedReader(new FileReader(inputFilePath))) {
+//
+//            String line;
+//
+//            // Flag to indicate if we are inside the <tbody> tag
+//            boolean insideTbody = false;
+//
+//            // Iterate through the input HTML file
+//            while ((line = br.readLine()) != null) {
+//                // Check if we are inside the <tbody> tag
+//                if (line.contains("<tfoot>")) {
+//                    insideTbody = true;
+//                    line = br.readLine();
+//                }
+//                // Check if we are inside a <tr> element
+//                if (insideTbody && line.contains("<tr>")) {
+//                    line = br.readLine();
+//                    if (line != null) {
+//                        int entryIndex = 1;
+//                        DependencyUsage packageUsage = new DependencyUsage();
+//                        while ((line = br.readLine()) != null) {
+//                            //trContent.append(line).append("\n");
+//                            if (line.contains("</tr>")) {
+//                                break; // Stop processing when encountering </tr>
+//                            }
+//                            if (entryIndex > 0 && matchedDep.getId() != null) {
+//                                extractUsage(line, entryIndex, matchedDep, packageUsage);
+//                            }
+//                            entryIndex++;
+//                        }
+//                        matchedDep.packageUsageMap.put(packageName, packageUsage);
+//                        break;
+//                    }
+//                }
+//            }
+//        }
+//    }
+
+
 
     private static Document createPackageReport(Element packageElement) throws Exception {
         DocumentBuilderFactory dbFactory = DocumentBuilderFactory.newInstance();
@@ -107,7 +175,7 @@ public class JacocoXMLParser {
         return doc;
     }
 
-    private static void writeXML(Document doc, String outputPath) throws Exception {
+    private static void writePackageXMLReport(Document doc, String outputPath) throws Exception {
         File outputFile = new File(outputPath);
         File parentDir = outputFile.getParentFile();
 
@@ -116,12 +184,63 @@ public class JacocoXMLParser {
             throw new IllegalStateException("Couldn't create directory: " + parentDir);
         }
 
+        // Format the XML
+        Document formattedDoc = formatXmlPackageReport(doc);
+
+        // Write the formatted XML to the file
         TransformerFactory transformerFactory = TransformerFactory.newInstance();
         Transformer transformer = transformerFactory.newTransformer();
-        DOMSource source = new DOMSource(doc);
+
+        // Set indentation properties
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4"); // Indentation size
+
+        DOMSource source = new DOMSource(formattedDoc);
         StreamResult result = new StreamResult(outputFile);
         transformer.transform(source, result);
+
+        //System.out.println("Formatted XML has been written to: " + outputFile.getAbsolutePath());
     }
+
+    private static Document formatXmlPackageReport(Document doc) throws Exception {
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+
+        // Set indentation properties
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4"); // Indentation size
+
+        // Write the DOM document to a new DOMSource to perform the formatting
+        DOMSource source = new DOMSource(doc);
+        StreamResult result = new StreamResult(new java.io.StringWriter());
+        transformer.transform(source, result);
+
+        // Parse the formatted XML back to a new Document
+        DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+        factory.setFeature("http://apache.org/xml/features/nonvalidating/load-external-dtd", false);
+        DocumentBuilder builder = factory.newDocumentBuilder();
+        java.io.ByteArrayInputStream input = new java.io.ByteArrayInputStream(result.getWriter().toString().getBytes());
+        return builder.parse(input);
+    }
+
+    private static void formatXml(File file, Document doc) throws Exception {
+        TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        Transformer transformer = transformerFactory.newTransformer();
+
+        // Set indentation properties
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4"); // Indentation size
+
+        // Write the DOM document to the file
+        DOMSource source = new DOMSource(doc);
+        StreamResult result = new StreamResult(file);
+        transformer.transform(source, result);
+
+        System.out.println("Formatted XML has been written to: " + file.getAbsolutePath());
+    }
+
+
+
 }
 
 
