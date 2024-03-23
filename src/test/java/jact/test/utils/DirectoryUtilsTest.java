4a623eb00d29e0e1943d0691b4f97df364f7e356
Jonas Sävås <jonas.savas123@gmail.com>
2024-03-23 12:46:53 +0100

    Adds tests for DirectoryUtils

--- src/test/java/jact/test/utils/DirectoryUtilsTest.java
+++ src/test/java/jact/test/utils/DirectoryUtilsTest.java
@@ -1,4 +1,83 @@
 package jact.test.utils;
 
+import jact.utils.CommandExecutor;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static jact.utils.CommandExecutor.copyPNGImage;
+import static jact.utils.DirectoryUtils.*;
+import static junit.framework.TestCase.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import jact.utils.DirectoryUtils;
+
+import java.io.File;
+import java.io.IOException;
+
 public class DirectoryUtilsTest {
+
+    static String testDirectory = "./src/test/java/jact/test/testingDir/";
+
+    @AfterAll
+    public static void cleanUpTestDirs(){
+        removeDirectory(new File(testDirectory));
+        Assertions.assertFalse(new File(testDirectory).exists());
+    }
+
+    @Test
+    public void yourTestMethod() {
+        // Your test logic goes here
+        assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void createAndRemoveEmptyDirectoryTest(){
+        Assertions.assertFalse(new File(testDirectory + "createdDir").exists());
+        createDir(testDirectory + "createdDir");
+        assertTrue(new File(testDirectory + "createdDir").exists());
+
+        removeDirectory(new File(testDirectory + "createdDir"));
+        Assertions.assertFalse(new File(testDirectory + "createdDir").exists());
+    }
+
+    @Test
+    public void createAndRemoveDirectoryTest() throws IOException {
+        createDir(testDirectory + "createdDir");
+        assertTrue(new File(testDirectory + "createdDir").exists());
+        Assertions.assertFalse(new File(testDirectory + "createdDir" + "/jact-logo.png").exists());
+        copyPNGImage("jact-logo.png", testDirectory + "createdDir");
+        assertTrue(new File(testDirectory + "createdDir" + "/jact-logo.png").exists());
+
+        // Recursive deletion of files contained in the directory
+        removeDirectory(new File(testDirectory + "createdDir"));
+        Assertions.assertFalse(new File(testDirectory + "createdDir" + "/jact-logo.png").exists());
+        Assertions.assertFalse(new File(testDirectory + "createdDir").exists());
+    }
+
+
+    @Test
+    public void copyAndMoveDirectoryTest() throws IOException {
+        createDir(testDirectory + "createdDir");
+        assertTrue(new File(testDirectory + "createdDir").exists());
+        copyPNGImage("jact-logo.png", testDirectory + "createdDir");
+        assertTrue(new File(testDirectory + "createdDir" + "/jact-logo.png").exists());
+
+        // Recursive deletion of files contained in the directory
+        createDir(testDirectory + "movedDir");
+        moveDirectory(new File(testDirectory + "createdDir"), testDirectory + "movedDir");
+        Assertions.assertFalse(new File(testDirectory + "createdDir" + "/jact-logo.png").exists());
+        Assertions.assertFalse(new File(testDirectory + "createdDir").exists());
+        assertTrue(new File(testDirectory + "/movedDir" +"/createdDir").exists());
+        assertTrue(new File(testDirectory + "/movedDir" +"/createdDir" + "/jact-logo.png").exists());
+
+
+        // Remove the moved directory
+        removeDirectory(new File(testDirectory + "movedDir"));
+        Assertions.assertFalse(new File(testDirectory + "/movedDir" +"/createdDir" + "/jact-logo.png").exists());
+        Assertions.assertFalse(new File(testDirectory + "/movedDir" +"/createdDir").exists());
+    }
 }
