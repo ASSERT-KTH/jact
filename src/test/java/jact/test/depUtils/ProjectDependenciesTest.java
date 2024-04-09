c96f2028153d131d41ff6ac4f7dd4ec3534a1b20
Jonas Sävås <jonas.savas123@gmail.com>
2024-04-09 17:19:12 +0200

    Adds a rigorous brute force test for dependency report paths

--- src/test/java/jact/test/depUtils/ProjectDependenciesTest.java
+++ src/test/java/jact/test/depUtils/ProjectDependenciesTest.java
@@ -172,4 +172,112 @@ public class ProjectDependenciesTest {
     }
 
 
+    @Test
+    public void reportPathsTest(){
+        // Brute force check that all dependencies have their
+        // manually checked report paths
+        ProjectDependency currDep = dependencies.get("com.google.guava:guava:33.0.0-jre");
+        assertEquals(1, currDep.getReportPaths().size());
+        assertEquals("dependencies/com.google.guava.guava-v33.0.0-jre", currDep.getReportPaths().get(0));
+
+
+        currDep = dependencies.get("com.google.code.findbugs:jsr305:3.0.2");
+        assertEquals(1, currDep.getReportPaths().size());
+        assertEquals("dependencies/com.google.guava.guava-v33.0.0-jre/" +
+                        "transitive-dependencies/" +
+                        "com.google.code.findbugs.jsr305-v3.0.2",
+                currDep.getReportPaths().get(0));
+
+
+        currDep = dependencies.get("com.google.errorprone:error_prone_annotations:2.23.0");
+        assertEquals(1, currDep.getReportPaths().size());
+        assertEquals("dependencies/com.google.guava.guava-v33.0.0-jre/" +
+                        "transitive-dependencies/" +
+                        "com.google.errorprone.error_prone_annotations-v2.23.0",
+                currDep.getReportPaths().get(0));
+
+
+        currDep = dependencies.get("com.google.guava:failureaccess:1.0.2");
+        assertEquals(1, currDep.getReportPaths().size());
+        assertEquals("dependencies/com.google.guava.guava-v33.0.0-jre/" +
+                        "transitive-dependencies/" +
+                        "com.google.guava.failureaccess-v1.0.2",
+                currDep.getReportPaths().get(0));
+
+        currDep = dependencies.get("com.google.guava:listenablefuture:9999.0-empty-to-avoid-conflict-with-guava");
+        assertEquals(1, currDep.getReportPaths().size());
+        assertEquals("dependencies/com.google.guava.guava-v33.0.0-jre/" +
+                        "transitive-dependencies/" +
+                        "com.google.guava.listenablefuture-v9999.0-empty-to-avoid-conflict-with-guava",
+                currDep.getReportPaths().get(0));
+
+
+        currDep = dependencies.get("com.google.j2objc:j2objc-annotations:2.8");
+        assertEquals(1, currDep.getReportPaths().size());
+        assertEquals("dependencies/com.google.guava.guava-v33.0.0-jre/" +
+                        "transitive-dependencies/" +
+                        "com.google.j2objc.j2objc.annotations-v2.8",
+                currDep.getReportPaths().get(0));
+
+        currDep = dependencies.get("org.checkerframework:checker-qual:3.41.0");
+        assertEquals(1, currDep.getReportPaths().size());
+        assertEquals("dependencies/com.google.guava.guava-v33.0.0-jre/" +
+                        "transitive-dependencies/" +
+                        "org.checkerframework.checker.qual-v3.41.0",
+                currDep.getReportPaths().get(0));
+
+
+        currDep = dependencies.get("joda-time:joda-time:2.12.7");
+        assertEquals(1, currDep.getReportPaths().size());
+        assertEquals("dependencies/joda.time.joda.time-v2.12.7", currDep.getReportPaths().get(0));
+
+        currDep = dependencies.get("junit:junit:4.13.2");
+        assertEquals(1, currDep.getReportPaths().size());
+        assertEquals("dependencies/junit.junit-v4.13.2", currDep.getReportPaths().get(0));
+
+
+        currDep = dependencies.get("org.hamcrest:hamcrest-core:1.3");
+        assertEquals(1, currDep.getReportPaths().size());
+        assertEquals("dependencies/junit.junit-v4.13.2/" +
+                        "transitive-dependencies/" +
+                        "org.hamcrest.hamcrest.core-v1.3",
+                currDep.getReportPaths().get(0));
+
+        currDep = dependencies.get("org.apache.commons:commons-math3:3.6.1");
+        assertEquals(1, currDep.getReportPaths().size());
+        assertEquals("dependencies/org.apache.commons.commons.math3-v3.6.1", currDep.getReportPaths().get(0));
+
+        currDep = dependencies.get("org.junit.jupiter:junit-jupiter-api:5.10.2");
+        assertEquals(1, currDep.getReportPaths().size());
+        assertEquals("dependencies/org.junit.jupiter.junit.jupiter.api-v5.10.2", currDep.getReportPaths().get(0));
+
+        currDep = dependencies.get("org.apiguardian:apiguardian-api:1.1.2");
+        assertEquals(2, currDep.getReportPaths().size());
+        assertEquals("dependencies/org.junit.jupiter.junit.jupiter.api-v5.10.2/" +
+                        "transitive-dependencies/" +
+                        "org.apiguardian.apiguardian.api-v1.1.2",
+                currDep.getReportPaths().get(0));
+        assertEquals("dependencies/org.junit.jupiter.junit.jupiter.api-v5.10.2/" +
+                        "transitive-dependencies/" +
+                        "org.junit.platform.junit.platform.commons-v1.10.2/" +
+                        "transitive-dependencies/" +
+                        "org.apiguardian.apiguardian.api-v1.1.2",
+                currDep.getReportPaths().get(1));
+
+        currDep = dependencies.get("org.junit.platform:junit-platform-commons:1.10.2");
+        assertEquals(1, currDep.getReportPaths().size());
+        assertEquals("dependencies/org.junit.jupiter.junit.jupiter.api-v5.10.2/" +
+                        "transitive-dependencies/" +
+                        "org.junit.platform.junit.platform.commons-v1.10.2",
+                currDep.getReportPaths().get(0));
+
+        currDep = dependencies.get("org.opentest4j:opentest4j:1.3.0");
+        assertEquals(1, currDep.getReportPaths().size());
+        assertEquals("dependencies/org.junit.jupiter.junit.jupiter.api-v5.10.2/" +
+                        "transitive-dependencies/" +
+                        "org.opentest4j.opentest4j-v1.3.0",
+                currDep.getReportPaths().get(0));
+    }
+
+
 }
